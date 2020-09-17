87560f04bcbf19b49341a3509756a212d037f772
mqmalagris <malagrismatheus@gmail.com>
2020-09-17 18:33:11 -0300

    firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
 
-    var firebaseConfig = {
-      apiKey: "AIzaSyAn_2CCgXKwJrFMWQ9rj6moQpZ_FFFEWBg",
-      authDomain: "jta-instagram-clone-2f4d8.firebaseapp.com",
-      databaseURL: "https://jta-instagram-clone-2f4d8.firebaseio.com",
-      projectId: "jta-instagram-clone-2f4d8",
-      storageBucket: "jta-instagram-clone-2f4d8.appspot.com",
-      messagingSenderId: "640207690050",
-      appId: "1:640207690050:web:e2cfd93e427d886f5610db",
-      measurementId: "G-KCF5ED6Z8J"
-    };
+    var firebaseConfig = firebaseConfigs;
 
     firebase.initializeApp(firebaseConfig);
   }
